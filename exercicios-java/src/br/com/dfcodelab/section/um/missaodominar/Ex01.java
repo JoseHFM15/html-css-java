b47e0cd3bf8792734c09eacf7a6c1f3cc9e6f906
JoseHFM15 <josehenrique150213@gmail.com>
2026-01-07 14:08:31 -0300

    conclusao do ex01

--- exercicios-java/src/br/com/dfcodelab/section/um/missaodominar/Ex01.java
+++ exercicios-java/src/br/com/dfcodelab/section/um/missaodominar/Ex01.java
@@ -1,10 +1,20 @@
 package br.com.dfcodelab.section.um.missaodominar;
 
+import java.util.Scanner;
+
 public class Ex01 {
 
     public static void main(String[] args) {
 
-        
+        Scanner ler = new Scanner(System.in);
+        int num = ler.nextInt();
+        int num2 = ler.nextInt();
+        int num3 = ler.nextInt();
+
+        int soma = num + num2 + num3;
+
+        float media = (float) soma / 3;
+        System.out.println(media);
 
     }
 
---
+++ exercicios-java/src/br/com/dfcodelab/section/um/missaodominar/Ex02.java
@@ -0,0 +1,16 @@
+package br.com.dfcodelab.section.um.missaodominar;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in)
+
+
+                
+        double valorCompra = ler.nextDouble();
+        double valorEntregue = ler.nextDouble();
+
+    }
+}
