76ea5b30ea7f81ff1623af9ed90e052320813208
JoseHFM15 <josehenrique150213@gmail.com>
2026-01-07 14:44:40 -0300

    ex04 finalizado

---
+++ exercicios-java/src/br/com/dfcodelab/section/um/missaodominar/ex04.java
@@ -0,0 +1,19 @@
+package br.com.dfcodelab.section.um.missaodominar;
+
+import java.util.Scanner;
+
+public class ex04 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("digite o valor do produto: ");
+        double valor = ler.nextDouble();
+        System.out.println("digite o valor do desconto: ");
+        int desconto = ler.nextInt();
+        double valdesc = desconto * valor / 100;
+        System.out.println("o valor do desconto é: " + valdesc);
+
+        double valCDesc = valor - valdesc;
+        System.out.println("o valor com desconto é: " + valCDesc);
+
+    }
+}
