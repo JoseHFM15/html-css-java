74c8af51594b55fe65b6213b7ebbb78b40aff074
JoseHFM15 <josehenrique150213@gmail.com>
2026-01-07 16:30:16 -0300

    ex09 finalizado

---
+++ exercicios-java/src/br/com/dfcodelab/section/um/missaodominar/ex09.java
@@ -0,0 +1,19 @@
+package br.com.dfcodelab.section.um.missaodominar;
+
+import java.util.Scanner;
+
+public class ex09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("digite a altura do triangulo: ");
+        double altura = input.nextDouble();
+
+        System.out.println("digite a base do triangulo: ");
+        double base = input.nextDouble();
+
+        double area = base * altura / 2;
+
+        System.out.println("a area do triangulo é: " + area);
+    }
+}
