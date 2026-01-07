8b8e56e5a694c34335052575d7192e4bf310ef52
JoseHFM15 <josehenrique150213@gmail.com>
2026-01-07 15:27:57 -0300

    ex06 finalizado

---
+++ exercicios-java/src/br/com/dfcodelab/section/um/missaodominar/ex06.java
@@ -0,0 +1,34 @@
+package br.com.dfcodelab.section.um.missaodominar;
+
+
+
+import java.util.Scanner;
+
+        public class ex06 {
+            public static void main(String[] args) {
+
+
+                Scanner input = new Scanner(System.in);
+
+                System.out.println("digite a base: ");
+                int base = input.nextInt();
+
+                System.out.println("digite o expoente: ");
+                int exp = input.nextInt();
+
+                int resultado = 1;
+
+                for (int i = 0; i < exp; i++) {
+
+                    resultado *= base;;
+                    System.out.println("o resultado é:" + resultado);
+
+                }
+
+
+
+
+
+            }
+        }
+
