d6ba8fcfa38a8cf54bc4b15459c36b6b6e15df74
JoseHFM15 <josehenrique150213@gmail.com>
2026-01-07 14:19:58 -0300

    ex02 finalizado

--- exercicios-java/src/br/com/dfcodelab/section/um/missaodominar/Ex02.java
+++ exercicios-java/src/br/com/dfcodelab/section/um/missaodominar/Ex02.java
@@ -5,12 +5,25 @@ import java.util.Scanner;
 public class Ex02 {
     public static void main(String[] args) {
 
-        Scanner ler = new Scanner(System.in)
+        Scanner ler = new Scanner(System.in);
 
 
-                
+        System.out.println("Digite o valor de sua compra: ");
         double valorCompra = ler.nextDouble();
+        System.out.println("Digite o valor que voce pagou:");
         double valorEntregue = ler.nextDouble();
+
+        if (valorCompra < valorEntregue) {
+            double troco = valorEntregue - valorCompra;
+            System.out.println("seu troco sera de " + troco);
+        }else if (valorCompra == valorEntregue) {
+            System.out.println("não recebera troco");
+        }else {
+
+            double devendo = valorCompra - valorEntregue;
+
+            System.out.println("voce esta devendo " + devendo);
+        }
 
     }
 }
